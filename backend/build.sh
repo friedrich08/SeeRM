d6280009b0594198b8110efb5882ac1b75ef6b06
Friedrich08 <Friedrichdawanou08@gmail.com>
2026-04-26 07:25:46 +0000

    Python 3.11 forcing

--- backend/build.sh
+++ backend/build.sh
@@ -2,6 +2,11 @@
 # exit on error
 set -o errexit
 
+# Set CARGO_HOME to a writable directory for Rust-based packages
+export CARGO_HOME=$HOME/.cargo
+
+python --version
+pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
