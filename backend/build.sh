700cd4d369af90e900647318b1930322c884e1cc
Friedrich08 <Friedrichdawanou08@gmail.com>
2026-04-26 07:55:37 +0000

    Lock Django to 5.1 and clean requirements

--- backend/build.sh
+++ backend/build.sh
@@ -2,12 +2,7 @@
 # exit on error
 set -o errexit
 
-# Set CARGO_HOME to a writable directory for Rust-based packages
-export CARGO_HOME=$HOME/.cargo
-
-python --version
-pip install --upgrade pip setuptools wheel
-pip install python-bidi==0.4.2
+python -m pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
